8da51c637debd5e569916fd953b6e3be6e4c8bcd
Medha20s <75370974+Medha20s@users.noreply.github.com>
2021-05-24 20:50:35 +0530

    Add files via upload

---
+++ Online Interviewer .py
@@ -0,0 +1,33 @@
+p=int(input("What is your age?"))
+print("You are",p,"years old.")
+
+u=int(input("Which class are you in?"))
+print("You are in class",u)
+
+i=int(input("In which year were you born?"))
+print("You were born in the year",i)
+
+r=int(input("What is your favourite number?"))
+print("Your favourite number is",r)
+
+e=int(input("How many siblings do you have?"))
+print("You have",e,"sibling/siblings.")
+
+f=int(input("How many teachers do you have?"))
+print("You have",f,"teachers.")
+
+g=int(input("What is the strength of your class?"))
+print("There are",g,"students in your class.")
+
+print("So you are",p,"Years old,"
+      " you are in class",u,"in school,"
+      " in the year",i,"you were born,"
+      " the number",r,"is your favourite,"
+      " you have",e,"sibling/siblings,"
+      " you have",f,"teachers &"
+      " you have",g,"classmates.")
+
+
+
+
+      
